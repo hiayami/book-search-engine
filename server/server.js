938161ad6e5d80d23b33710b10b80ff5622293bc
Ayami Higashi <ayamibhigashi@gmail.com>
2022-07-21 21:37:42 -0700

    run graphql alongside REST server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+const routes = require("./routes");
+const { ApolloServer, gql } = require("apollo-server-express");
+const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
+const http = require("http");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,13 +12,56 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
 
-app.use(routes);
+const resolvers = {
+  Query: {
+    books: () => [
+      {
+        title: "The Awakening",
+        author: "Kate Chopin",
+      },
+      {
+        title: "City of Glass",
+        author: "Paul Auster",
+      },
+    ],
+  },
+};
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: "bounded",
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
+
+const startServer = async () => {
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: "/",
+  });
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
+
+  app.use(routes);
+  await new Promise((resolve) => db.once("open", resolve));
+  await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+  console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
